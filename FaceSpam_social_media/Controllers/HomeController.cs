320ed662d0ce1e1e3b2fbd1c611e314c580c1522
SeredaYehor <sirjeleshka@gmail.com>
2022-03-24 19:27:16 +0300

    Added actions for main view

--- FaceSpam_social_media/Controllers/HomeController.cs
+++ FaceSpam_social_media/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace FaceSpam_social_media.Controllers
 {
@@ -13,6 +14,7 @@ namespace FaceSpam_social_media.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        public static Main mainFormModels = new Main();
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -38,6 +40,25 @@ namespace FaceSpam_social_media.Controllers
             ViewData["string"] = data; 
 
             return View();
+        }
+
+        public IActionResult Main()
+        {
+            mainFormModels.user = new User(1, "W1ld 3lf", "12345", "ogo@mail.com",
+            "Кодер на миллион", null);
+            mainFormModels.posts.Add(new Post(1, "Hi everyone!"));
+            mainFormModels.posts.Add(new Post(2, "Wats up?"));
+            mainFormModels.posts.Add(new Post(3, "This is an example post and bla bla bla" +
+                "bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla"));
+            return View(mainFormModels);
+        }
+
+        [HttpPost]
+        public IActionResult AddPost(Main model)
+        {
+            mainFormModels.posts.Add(new Post(4, model.message));
+            ModelState.Clear();
+            return View("Main", mainFormModels);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
