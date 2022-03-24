085074b418ef1627487a1bf81c6c0ef1c4f9b0e7
SeredaYehor <sirjeleshka@gmail.com>
2022-03-24 19:10:33 +0300

    Added actions for ajax functions

--- FaceSpam_social_media/Controllers/HomeController.cs
+++ FaceSpam_social_media/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace FaceSpam_social_media.Controllers
 {
@@ -13,11 +14,23 @@ namespace FaceSpam_social_media.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        public static Main mainFormModels = new Main();
+        public static UserChats group = new UserChats();
         public HomeController(ILogger<HomeController> logger)
         {
+            
             _logger = logger;
         }
 
+        [HttpPost]
+        public void SetId(int id)
+        {
+            group.chatId = id;
+        }
+        public int GetId()
+        {
+            return group.chatId;
+        }
         public IActionResult Index()
         {
             return View();
@@ -38,6 +51,52 @@ namespace FaceSpam_social_media.Controllers
             ViewData["string"] = data; 
 
             return View();
+        }
+
+        public IActionResult Main()
+        {
+            mainFormModels.user = new User(1, "W1ld 3lf", "12345", "ogo@mail.com",
+            "Кодер на миллион", null);
+            mainFormModels.posts.Add(new Post(1, "Hi everyone!"));
+            mainFormModels.posts.Add(new Post(2, "Wats up?"));
+            mainFormModels.posts.Add(new Post(3, "This is an example post and bla bla bla" +
+                "bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla"));
+            return View(mainFormModels);
+        }
+
+        public IActionResult Messages()
+        {
+            group.chats.Add(new Chat(1, "Dorenskiy O. P.", "Some chat", 2, DateTime.Now, "ooo"));
+            //Bug!!!  when adding messages chats also appends>(
+            group.chats[0].chatMessages.Add(new Message(4, "Prikol",
+                DateTime.Now, 1, 0, 1));
+            group.chats.Add(new Chat(2, "FaceSpam Community", "Some chat", 2, DateTime.Now, "ooo"));
+            group.chats[1].chatMessages.Add(new Message(4, "Woobshe",
+                DateTime.Now, 2, 0, 1));
+            group.chats.Add(new Chat(3, "Elon Musk", "Some chat", 2, DateTime.Now, "ooo"));
+            group.chats[2].chatMessages.Add(new Message(4, "Ulyot",
+               DateTime.Now, 3, 0, 1));
+            return View(group);
+        }
+
+        public void SendMessage(string textboxMessage)
+        {
+            group.chats[group.GetChatId(group.chatId)].chatMessages.Add(new Message(4, textboxMessage, 
+                DateTime.Now, 1, 0, group.GetChatId(group.chatId)));
+        }
+
+        public List<Message> GetChatMessages()
+        {
+            List<Message> result = group.chats[group.GetChatId(group.chatId)].chatMessages;
+            return result;
+        }
+
+        [HttpPost]
+        public IActionResult AddPost(Main model)
+        {
+            mainFormModels.posts.Add(new Post(4, model.message));
+            ModelState.Clear();
+            return View("Main", mainFormModels);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
