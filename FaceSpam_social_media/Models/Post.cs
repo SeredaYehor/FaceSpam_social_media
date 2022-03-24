1f6f5a4b31c572206ce072ad0ea742fc06d851e8
SeredaYehor <sirjeleshka@gmail.com>
2022-03-24 19:12:58 +0300

    Added model for post info

---
+++ FaceSpam_social_media/Models/Post.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FaceSpam_social_media.Models
+{
+    public class Post
+    {
+        private int id;
+        private string text;
+        private DateTime timePosting;
+        private string imageReference;
+
+        public Post(int inputId, string inputText,string inputReference = null)
+        {
+            id = inputId;
+            text = inputText;
+            timePosting = DateTime.Now;
+            imageReference = inputReference;
+        }
+
+        public string Text
+        {
+            get => text;
+        }
+
+        public DateTime TimePosting
+        {
+            get => timePosting;
+        }
+
+        public string Image
+        {
+            get => imageReference;
+        }
+    }
+}
