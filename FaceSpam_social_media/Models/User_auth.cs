fbe1f5b6676415e9fa702ff5f44cb07ab4e13ac5
SeredaYehor <sirjeleshka@gmail.com>
2022-03-24 18:52:15 +0300

    Added example model

---
+++ FaceSpam_social_media/Models/User_auth.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FaceSpam_social_media.Models
+{
+    //Model is useful to storage data from post requests eg. textbox
+    public class User_auth
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
