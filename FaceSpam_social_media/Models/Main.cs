d3f318537b05a65f1ba4b98eaaeed9fafd1d3185
SeredaYehor <sirjeleshka@gmail.com>
2022-03-24 19:23:15 +0300

    Added model for main form

---
+++ FaceSpam_social_media/Models/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FaceSpam_social_media.Models
+{
+    public class Main
+    {
+        public User user;
+        public List<Post> posts = new List<Post>();
+        public string message { get; set; }
+    }
+}
