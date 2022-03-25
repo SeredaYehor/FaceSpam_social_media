ec5ead9e4e87d35a17eb4a22ab21fb83a22fee18
Nebesnyi Andrew <73758238+artpro197@users.noreply.github.com>
2022-03-25 19:00:04 +0300

    Added test models for Friends view

---
+++ FaceSpam_social_media/Models/FriendsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FaceSpam_social_media.Models
+{
+    public class FriendsModel
+    {
+        // this model is just a template
+        // in some sprints it will be improved and supplemented
+
+        public List<UserModel> userName = new List<UserModel>();
+    }
+}
