3e09df373e65fab78b6a22055d90ca973286d0d2
Nebesnyi Andrew <73758238+artpro197@users.noreply.github.com>
2022-03-26 17:22:34 +0300

    Controller updated, write function was added

--- FaceSpam_social_media/Controllers/HomeController.cs
+++ FaceSpam_social_media/Controllers/HomeController.cs
@@ -12,6 +12,7 @@ namespace FaceSpam_social_media.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private static Post postModel = new Post("Vadim", "Best palce to cheel is 715", new DateTime(2020, 3, 11, 13, 0, 0));
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -23,21 +24,25 @@ namespace FaceSpam_social_media.Controllers
             return View();
         }
 
-        public IActionResult ControllerTest()
+        [HttpGet]
+        public IActionResult Comments(string message = null)   
         {
-            return View();
+            postModel.postComments.Add(new PostComment("CyberDemon", "2 years passed, where is lab 12", DateTime.Now));
+            postModel.postComments.Add(new PostComment("CyberDemon", "Also u forgot about Prat book buing", DateTime.Now));
+
+            return View(postModel);
         }
 
-        public IActionResult Privacy()
+        [HttpPost]
+        public IActionResult AddComment(string message)
         {
-            return View();
-        }
+            string name = "Vadim";
+            if (message != null)
+            {
+                postModel.postComments.Add(new PostComment(name, message, DateTime.Now));
+            }
 
-        public IActionResult ShowData(string data)
-        {
-            ViewData["string"] = data; 
-
-            return View();
+            return View("Comments", postModel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
