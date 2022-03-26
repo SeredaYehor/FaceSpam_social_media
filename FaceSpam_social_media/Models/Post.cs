b4bd0d530730afc4e0014eef2f0840bf0de2b3ed
Nebesnyi Andrew <73758238+artpro197@users.noreply.github.com>
2022-03-26 17:18:08 +0300

    Added test models for Comments view

---
+++ FaceSpam_social_media/Models/Post.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FaceSpam_social_media.Models
+{
+    public class Post
+    {
+        public Post(string name, string text, DateTime time)
+        {
+            userName = name;
+            postText = text;
+            postCreating = time;
+        }
+
+        private string userName;
+        private string postText;
+
+        private DateTime postCreating = new DateTime();
+
+        public List<PostComment> postComments = new List<PostComment>();
+        public string getUser
+        {
+            get => userName;
+        }
+
+        public string getMessage
+        {
+            get => postText;
+        }
+
+        public DateTime getTime
+        {
+            get => postCreating;
+        }
+    }
+}
---
+++ FaceSpam_social_media/Models/PostComment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FaceSpam_social_media.Models
+{
+    public class PostComment
+    {
+        private string userName;
+        private string postText;
+        private DateTime commentCreated;
+
+        public string getUser
+        {
+            get => userName;
+        }
+
+        public string getMessage
+        {
+            get => postText;
+        }
+
+        public DateTime getTime
+        {
+            get => commentCreated;
+        }
+
+        public PostComment(string name, string text, DateTime time)
+        {
+            userName = name;
+            postText = text;
+            commentCreated = time;
+        }
+    }
+}
