1aac5f3218f0a0aca993f4398b22e9d72a9624ec
SeredaYehor <sirjeleshka@gmail.com>
2022-03-29 14:58:15 +0300

    Added filter script

--- FaceSpam_social_media/wwwroot/scripts/MessagesScripts.js
+++ FaceSpam_social_media/wwwroot/scripts/MessagesScripts.js
@@ -30,13 +30,11 @@
                 success: function (messages) { //get array object of Message models
                     for (var index = 0; index < messages.length; index++) { //adding all messages for this chat
                         messageObject = '<div class="Message">' +
-                            '<svg class="Circle" viewBox="0 0 35 35">' +
-                            '<ellipse cx="15" cy="15" rx="15" ry="15" />' +
-                            '</svg>' +
-                            '<label style="position: relative; font-size: 14px; color: #3485FF;">W1ld 3lf</label>' +
+                            '<img src="../images/W1ld 3lf.jpg" class="Ellipse" style="width: 50px; height: 50px;"/>' +
+                            '<label style="margin-left: 3px; position: relative; font-size: 14px; color: #3485FF;">W1ld 3lf</label>' +
                             '<label style="display: inline-block; margin-left: 200px; font-size: 12px; width: 40px; color: #3485FF; text-align: right;">' + messages[index]["time"].toString() + '</label > ' +
                             '<br />' +
-                            '<label style="font-size: 12px; position: relative; left: 10px; color: #3485FF; max-width: 500px; overflow: hidden; word-wrap: break-word;">' + messages[index]["text"].toString() + '</label > ' +
+                            '<label style="margin-top: 5px; font-size: 12px; position: relative; left: 10px; color: #3485FF; max-width: 500px; overflow: hidden; word-wrap: break-word;">' + messages[index]["text"].toString() + '</label > ' +
                             '</div>';
                         $(".ChatMessages").append(messageObject);
                     }
@@ -54,15 +52,18 @@
             });
 
             messageObject = '<div class="Message">' +
-                '<svg class="Circle" viewBox="0 0 35 35">' +
-                '<ellipse cx="15" cy="15" rx="15" ry="15" />' +
-                '</svg>' +
-                '<label style="position: relative; font-size: 14px; color: #3485FF;">W1ld 3lf</label>' +
+                '<img src="../images/W1ld 3lf.jpg" class="Ellipse" style="width: 50px; height: 50px;"/>' +
+                '<label style="margin-left: 3px; position: relative; font-size: 14px; color: #3485FF;">W1ld 3lf</label>' +
                 '<label style="display: inline-block; margin-left: 200px; font-size: 12px; width: 40px; color: #3485FF; text-align: right;">12.04</label>' +
                 '<br />' +
-                '<label style="font-size: 12px; position: relative; left: 10px; color: #3485FF; max-width: 500px; overflow: hidden; word-wrap: break-word;">' + message + '</label > ' +
+                '<label style="margin-top: 5px; font-size: 12px; position: relative; left: 10px; color: #3485FF; max-width: 500px; overflow: hidden; word-wrap: break-word;">' + message + '</label > ' +
                 '</div>'; //class of message
 
             $(".ChatMessages").append(messageObject); //add class to chat form
         }
+
+        $(".SearchTextBox").keyup(function () {
+            var search = this.value;
+            $(".GroupPanel").hide().filter(':contains(' + search + ')').show();
+        })
     });
