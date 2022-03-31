f1c57827f359e14bff4ef1238b6e054d64af4947
SeredaYehor <sirjeleshka@gmail.com>
2022-03-31 13:25:40 +0300

    Added jquery scripts for main form

---
+++ FaceSpam_social_media/wwwroot/scripts/MainFormScripts.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $(".HeartImage").click(function () {
+        var postId = $(this).attr("id");
+        var newImg = '';
+        if ($(this).attr("src") == "/images/heartRed.svg") {
+            newImg = '<img src="/images/heart.svg" class="HeartImage" id=' + postId + '/>';
+        }
+        else {
+            newImg = '<img src="/images/heartRed.svg" class="HeartImage" id=' + postId + '/>';
+        }
+        $(this).replaceWith(newImg);
+    })
+});
