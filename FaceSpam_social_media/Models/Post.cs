d17762a15b8a20e6e4a62b02da1dfe92a40755a4
SeredaYehor <sirjeleshka@gmail.com>
2022-03-31 13:23:32 +0300

    Added list for representing likes

--- FaceSpam_social_media/Models/Post.cs
+++ FaceSpam_social_media/Models/Post.cs
@@ -11,6 +11,7 @@ namespace FaceSpam_social_media.Models
         private string text;
         private DateTime timePosting;
         private string imageReference;
+        private List<int> userLikes = new List<int>();
 
         public Post(int inputId, string inputText,string inputReference = null)
         {
@@ -20,9 +21,43 @@ namespace FaceSpam_social_media.Models
             imageReference = inputReference;
         }
 
+        public void UpdateLike(int userId)
+        {
+            if(CheckLike(userId))
+            {
+                userLikes.Remove(userId);
+            }
+            else
+            {
+                userLikes.Add(userId);
+            }
+        }
+
+        public int CountLikes()
+        {
+            return userLikes.Count;
+        }
+
+        public bool CheckLike(int userId)
+        {
+            if(userLikes.Contains(userId))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
         public string Text
         {
             get => text;
+        }
+
+        public int ID
+        {
+            get => id;
         }
 
         public string TimePosting
