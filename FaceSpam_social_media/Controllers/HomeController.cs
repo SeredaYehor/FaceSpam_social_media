e3927479746577f33bf15be5868095210918904b
Nebesnyi Andrew <73758238+artpro197@users.noreply.github.com>
2022-04-09 19:37:20 +0300

    Removed bad controller of time adding

--- FaceSpam_social_media/Controllers/HomeController.cs
+++ FaceSpam_social_media/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace FaceSpam_social_media.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private static Post postModel = new Post("W1ld3lf", "Check out this view!!!", GetCurrentTime(), "W1ld3lf.png", "post.jpg");
+        private static Post postModel = new Post("W1ld3lf", "Check out this view!!!", DateTime.Now, "W1ld3lf.png", "post.jpg");
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -21,8 +21,8 @@ namespace FaceSpam_social_media.Controllers
 
         public IActionResult Index()
         {
-            postModel.postComments.Add(new PostComment("Elon Mask", "Great post-nuclear avantgarde view)", GetCurrentTime(), "Mask.jpg"));
-            postModel.postComments.Add(new PostComment("Митрополит Вадим", "Господь, господь", GetCurrentTime(), "Vadim.png"));
+            postModel.postComments.Add(new PostComment("Elon Mask", "Great post-nuclear avantgarde view)", DateTime.Now, "Mask.jpg"));
+            postModel.postComments.Add(new PostComment("Митрополит Вадим", "Господь, господь", DateTime.Now, "Vadim.png"));
 
             return View();
         }
@@ -36,18 +36,10 @@ namespace FaceSpam_social_media.Controllers
         {
             if (message != null)
             {
-                postModel.postComments.Add(new PostComment("W1ld3lf", message, GetCurrentTime(), "W1ld3lf.png"));
+                postModel.postComments.Add(new PostComment("W1ld3lf", message, DateTime.Now, "W1ld3lf.png"));
             }
 
             return View("Comments", postModel);
-        }
-
-        static public string GetCurrentTime()
-        {
-            string date = DateTime.Now.Date.ToString().Remove(5, 13) + "." + DateTime.Now.Year.ToString().Remove(0, 2);
-            string time = DateTime.Now.ToShortTimeString();
-
-            return date + " " + time;
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
