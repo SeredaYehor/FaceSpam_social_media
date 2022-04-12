354be1c0fd33f77ca0d657022c2a49168fafae8a
SeredaYehor <sirjeleshka@gmail.com>
2022-04-12 18:28:21 +0300

    Added untracked files

---
+++ FaceSpam_social_media/Models/PostComment.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FaceSpam_social_media.Models
+{
+    public class PostComment
+    {
+        private string userName;
+        private string postText;
+        private DateTime commentCreated;
+        private string userImage;
+        public PostComment(string name, string text, DateTime time, string image)
+        {
+            userName = name;
+            postText = text;
+            commentCreated = time;
+            userImage = image;
+        }
+        public string getUser
+        {
+            get => userName;
+        }
+
+        public string getMessage
+        {
+            get => postText;
+        }
+
+        public string getTime
+        {
+            get => commentCreated.ToString("dd.MM.y H:m");
+        }
+
+        public string getUserImage
+        {
+            get => userImage;
+        }
+    }
+}
---
+++ FaceSpam_social_media/wwwroot/scripts/CommentsScriptFile.js
@@ -0,0 +1,38 @@
+﻿$(document).ready(function () {
+});
+
+    function GetMessage(message, time) {
+        messageObject =
+            '<div style="display: flex; align-items: center; flex-direction: row">' +
+            '<img src="../Images/W1ld 3lf.jpg" class="UserImage" />' +
+            '<label class="UserName"> W1ld3lf </label>' +
+            '<label class="Time">' + time + '</label>' +
+            '</div>' +
+            '<div>' +
+            '<label class="PostedMessage">' + message + '</label>' +
+            '</div>';
+        $("#comments").append(messageObject);
+    }
+
+    function AddMessage() {
+        var text = document.getElementById("message").value;
+
+        $.ajax({
+            type: "GET",
+            url: "/Home/AddComment",
+            async: false,
+            data: { message: text, },
+        });
+
+        document.getElementById("message").value = null;
+
+        var time = GetDateTime();
+        GetMessage(text, time);
+    }
+
+    function GetDateTime() {
+        var date = new Date().toLocaleDateString().substr(0, 6) + new Date().getFullYear().toString().substr(-2);
+        var time = new Date().toLocaleTimeString().substr(0, 5);
+
+        return date + ' ' + time;
+    }
