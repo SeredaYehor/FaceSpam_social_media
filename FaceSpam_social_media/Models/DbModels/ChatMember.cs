c8f1e05e405d95902ab64121d14439ae84708f51
Egor Sereda <74448293+SeredaYehor@users.noreply.github.com>
2022-04-14 20:29:33 +0300

    St 208 (#21)
    * Added models for working with db
    
    * Updated project file for working with libs

---
+++ FaceSpam_social_media/Models/DbModels/ChatMember.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace FaceSpam_social_media.Models.DbModels
+{
+    public partial class ChatMember
+    {
+        public int UserUserId { get; set; }
+        public int ChatChatId { get; set; }
+
+        public virtual Chat ChatChat { get; set; }
+        public virtual User UserUser { get; set; }
+    }
+}
---
+++ FaceSpam_social_media/Models/DbModels/Like.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace FaceSpam_social_media.Models.DbModels
+{
+    public partial class Like
+    {
+        public int LikeId { get; set; }
+        public int UserUserId { get; set; }
+        public int PostPostId { get; set; }
+        public int MessageMessageId { get; set; }
+
+        public virtual Message MessageMessage { get; set; }
+        public virtual User UserUser { get; set; }
+    }
+}
