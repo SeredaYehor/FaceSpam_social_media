55d3828d1b1385c534ef37c88a18ae6186f4dfcb
Nebesnyi Andrew <73758238+artpro197@users.noreply.github.com>
2022-04-17 19:16:46 +0300

    Created JS functions for friends view

---
+++ FaceSpam_social_media/wwwroot/scripts/FriendsFormScripts.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+
+    $(".Search").keyup(function () {
+        var search = this.value;
+        if (search.trim() == '') {
+            $(".FriendLine").show();
+        }
+        else {
+            $(".FriendLine").hide().filter('[id^=' + search + ']').show();
+        }
+    });
+});
+
+function DeleteFriend(obj) {
+
+    var user = obj.id;
+    $.ajax({
+        type: "GET",
+        url: "/Home/DeleteFriend",
+        async: false,
+        data: { id: user, },
+        success: function () {
+            var parentDiv = $(obj).parent().parent();
+            $(parentDiv).remove();
+        }
+    });
+}
