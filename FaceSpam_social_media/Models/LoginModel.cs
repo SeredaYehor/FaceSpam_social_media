2b278b743deb7bca5fb2c8e0662cba35ed412d3d
Maxim-Drozdov <mr.emeraldgame@Gmail.com>
2022-04-26 12:27:03 +0300

    Added interaction with the database for the login form. (#26)
    * Added interaction with the database for the login form.
    
    * Update HomeController.cs
    
    * Removed comments
    
    Co-authored-by: Egor Sereda <74448293+SeredaYehor@users.noreply.github.com>

---
+++ FaceSpam_social_media/Models/LoginModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FaceSpam_social_media.Models
+{
+    public class LoginModel
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public bool Verify(DbModels.mydbContext context)
+        {
+            bool result = context.Users.Any(x => x.Name == Login && x.Password == Password);
+
+            return result;
+        }
+
+    }
+
+}
