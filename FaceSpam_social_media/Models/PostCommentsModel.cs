b7cc1ba600a98126633dacbab6253c55de397058
Nebesnyi Andrew <73758238+artpro197@users.noreply.github.com>
2022-04-26 20:35:50 +0300

    Added comments form intergration with DB

---
+++ FaceSpam_social_media/Models/PostCommentsModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FaceSpam_social_media.Models
+{
+    public class PostCommentsModel
+    {
+        public DbModels.User user = new DbModels.User();
+        public DbModels.Post post = new DbModels.Post();
+        public List<DbModels.User> users = new List<DbModels.User>();
+
+        public void GetComments(DbModels.mydbContext context)
+        {
+            post.Messages = context.Messages.Where(x => x.PostPostId == post.PostId).ToList();
+
+            foreach (var comment in post.Messages)
+            {
+                users.Add(context.Users.Where(x => x.UserId == comment.UserUserId).FirstOrDefault());
+            }
+        }
+
+        public void AddComment(DbModels.mydbContext context, string message)
+        {
+            DbModels.Message newMessage = new DbModels.Message();
+            newMessage.PostPostId = post.PostId;
+            newMessage.UserUserId = user.UserId;
+            newMessage.Text = message;
+            newMessage.DateSending = DateTime.Now;
+
+            context.Messages.Add(newMessage);
+            context.SaveChanges();
+        }
+    }
+}
