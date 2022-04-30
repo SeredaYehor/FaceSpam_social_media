19e7603773a420a5a1cb50a29f7bb6236c16ca2d
SeredaYehor <sirjeleshka@gmail.com>
2022-04-30 12:14:14 +0300

    Updated scripts for adding likes to new posts

--- FaceSpam_social_media/wwwroot/scripts/MainFormScripts.js
+++ FaceSpam_social_media/wwwroot/scripts/MainFormScripts.js
@@ -2,7 +2,11 @@
 
     var formData = new FormData();
 
-    $(".HeartImage").click(function ChangeColor() {
+    $("#Browse").click(function () {
+        document.getElementById('myFile').click();
+    })
+
+    $(".DashboardList").on("click", ".HeartImage", function ChangeColor() {
         if ($(this).attr("src") == "/images/heartRed.svg") {
             $(this).attr("src", "/images/heart.svg");
         }
@@ -12,15 +16,12 @@
         var id = $(this).attr("id");
         var likes = UpdateLikes(id);
         $(this).parent().children(".LikeLabel").html(likes);
-    })
-
-    $("#Browse").click(function () {
-        document.getElementById('myFile').click();
-    })
+    });
 
     $("#Write").click(function () {
         var text = $(".Post").val();
         formData.set('text', text);
+        $(".Post").val("");
         $.ajax({
             type: "POST",
             url: '/Home/AddPost',
@@ -28,12 +29,19 @@
             contentType: false,
             processData: false,
             success: function (user) {
-                AddNewPost(user, text, formData.get('file'));
+                AddNewPost(user["item1"], text, user["item2"], formData.get('file'));
             }
         });
     })
 
-    function AddNewPost(user, text, postImage = null) {
+    function GetCurrentDate() {
+        var dt = new Date();
+        var date = dt.getDate() + "." + ("0" + (dt.getMonth() + 1)).slice(-2) + "." + dt.getFullYear().toString().substr(2, 2);
+        return date;
+    }
+
+    function AddNewPost(user, text, postId, postImage = null) {
+        var date = GetCurrentDate();
         var postObject = '<div class="PostMessage">' +
             '<img src="' + user["imageReference"] + '"class="PostEllipse" />' +
             '<label style="position: relative; font-size: 18px; color: #3485FF;">' + user["name"] + '</label>' +
@@ -45,17 +53,16 @@
             }
         postObject += '<div style="background-color:transparent; height:50px;">' +
             '<div class="Likes" style="display: inline-block;">' +
-            '<img src="../images/heart.svg" class="HeartImage" id="@post.PostId" />' +
+            '<img src="../images/heart.svg" class="HeartImage" id="' + postId + '" />' +
             '<label class="LikeLabel">0</label>' +
             '</div><img src="../images/pencil.svg" style="display: inline-block; margin-left: 10px;" />' +
             '<label style="position: relative; display: inline-block; font-size: 12px; color: #3485FF;">comments 0</label>' +
-            '<label class="Date">' + 'choto' + '</label></div></div>';
-        $(".DashboardList").prepend(postObject);
+            '<label class="Date">' + date + '</label></div></div>';
+        $(".DashboardList").append(postObject);
     }
 
     $("#myFile").change(function () {
         var path = document.getElementById("myFile").files[0];
-        var tb_text = $(".Post").val();
         formData.set('file', path);
     })
 
