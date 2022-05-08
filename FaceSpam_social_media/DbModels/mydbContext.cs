2223d52ef6013b1a00ba471705d52776c66118ad
SeredaYehor <sirjeleshka@gmail.com>
2022-05-08 11:06:10 +0300

    Added new field

--- FaceSpam_social_media/DbModels/mydbContext.cs
+++ FaceSpam_social_media/DbModels/mydbContext.cs
@@ -29,7 +29,7 @@ namespace FaceSpam_social_media.DbModels
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseMySql("server=127.0.0.1;user=root;password=H3ll0W@rld;database=mydb", ServerVersion.Parse("8.0.25-mysql"));
+                optionsBuilder.UseMySql("server=127.0.0.1;user=root;password=Password;database=mydb", ServerVersion.Parse("8.0.25-mysql"));
             }
         }
 
@@ -254,6 +254,10 @@ namespace FaceSpam_social_media.DbModels
                 entity.Property(e => e.IsAdmin)
                     .HasMaxLength(1)
                     .HasColumnName("Is_admin");
+
+                entity.Property(e => e.IsBanned)
+                    .HasMaxLength(1)
+                    .HasColumnName("Is_banned");
 
                 entity.Property(e => e.Name)
                     .IsRequired()
