9a97c934c34121ec2f46cbfadfd47aa9791a75ed
Nebesnyi Andrew <73758238+artpro197@users.noreply.github.com>
2022-05-10 17:06:13 +0300

    Constructed function to add new friend, added is friend check

--- FaceSpam_social_media/Models/FriendsViewModel.cs
+++ FaceSpam_social_media/Models/FriendsViewModel.cs
@@ -10,7 +10,10 @@ namespace FaceSpam_social_media.Models
 
         public DbModels.User user;
         public List<DbModels.User> friends;
+        public List<DbModels.User> allUsers = new List<DbModels.User>();
         public int mainUserId;
+
+        public bool friendPage;
 
         public void GetUserById(DbModels.mydbContext context, int id)
         {
@@ -18,6 +21,12 @@ namespace FaceSpam_social_media.Models
 
             friends = context.Friends.Where(x => x.UserUserId == user.UserId)
                 .Select(x => x.FriendNavigation).ToList();
+        }
+
+        public void GetAllUsers(DbModels.mydbContext context)
+        {
+            allUsers = context.Users.ToList();
+            allUsers.Remove(allUsers.Where(x=>x.UserId == mainUserId).First());
         }
 
         public void DeleteFriend(DbModels.mydbContext context, int id)
@@ -29,6 +38,34 @@ namespace FaceSpam_social_media.Models
             context.Friends.Remove(friend);
             context.SaveChanges();
             friends.Remove(friends.Where(x=>x.UserId==id).FirstOrDefault());
+        }
+
+        public void AddFriend(DbModels.mydbContext context, int id)
+        {
+            DbModels.Friend newFriend = new DbModels.Friend();
+            newFriend.UserUserId = mainUserId;
+            newFriend.FriendId = id;
+
+            context.Friends.Add(newFriend);
+            context.SaveChanges();
+
+            friends.Add(context.Users.Where(x => x.UserId == id).FirstOrDefault());
+
+        }
+
+        public string IsFriend(int userId)
+        {
+            string result = "Pal up";
+            foreach(var user in friends)
+            {
+                if(user.UserId == userId)
+                {
+                    result = "Remove";
+                    break;
+                }
+            }         
+
+            return result;
         }
     }
 }
