fc03ee56f234e03c86e36d8bba6766729701a462
SeredaYehor <sirjeleshka@gmail.com>
2022-05-19 11:03:21 +0300

    Added scripts for groups view

---
+++ FaceSpam_social_media/wwwroot/scripts/GroupsFormScripts.js
@@ -0,0 +1,84 @@
+﻿$(document).ready(function() {
+    var addedMembers = Array();
+    var formData = new FormData();
+
+    $(".Upload").click(function () {
+        document.getElementById('myFile').click();
+    })
+
+    $("#myFile").change(function () {
+        var path = null;
+        path = document.getElementById("myFile").files[0];
+        if (path != null) {
+            $(".GroupImage").css("background-image", URL.createObjectURL(path));
+        }
+        formData.set('file', path);
+    })
+
+    $(".List").on('click', ".MemberActionButton", function () {
+        var action = $(this).attr("value").toString();
+        var id = $(this).attr("id").toString();
+        if (action == "Add") {
+            addedMembers.push(id);
+            action = "Rem";
+        }
+        else {
+            addedMembers.pop(id);
+            action = "Add";
+        }
+        $(this).attr("value", action);
+    })
+
+    function GenerateGroupPanel(id, image, name) {
+        var panel = '<div id="' + id + '" class="FriendLine" style="display: flex; align-items: center; flex-direction: row">' +
+            '<img class="UserImage" src="' + image + '" />' +
+            '<label class="Names">' + name + '</label>' +
+            '<input id="' + id + '" type="submit" class="FriendButton" value="Quit" onclick="DeleteChat(this)" />' +
+            '<input type="submit" class="FriendButton" value="Write" />' +
+            '</div>';
+        $(".GroupsPanels").append(panel);
+    }
+    
+    $(".ConfirmButton").click(function () {
+        formData.set("chatName", $("#name").val());
+        formData.set("chatDescription", $("#description").val());
+        formData.set("members", addedMembers);
+        $(".CreateGroup").hide();
+        $.ajax({
+            type: "POST",
+            url: '/Home/CreateGroup',
+            async: false,
+            data: formData,
+            success: function (group) {
+                GenerateGroupPanel(group["chatId"], group["imageReference"],
+                    group["chatName"]);
+            }
+        });
+    })
+
+    function AddUserPanel(id, name, image) {
+        var panel = '<div class="UserPanel">' +
+            '<img src="' + image + '" class="UserPanelImage" />' +
+            '<label>' + name + '</label>' +
+            '<input type="submit" class="MemberActionButton" value="Add" id="' + id + '"/>' +
+            '</div >';
+        $(".List").append(panel);
+    }
+
+    $(".CreateGroupButton").click(function () {
+        $(".CreateGroup").show();
+        $.ajax({
+            type: "POST",
+            url: '/Home/SelectUsers',
+            async: false,
+            success: function (list) {
+                for (var i = 0; i < list.length; i++) {
+                    var id = list[i]["userId"].toString();
+                    var name = list[i]["name"].toString();
+                    var image = list[i]["imageReference"].toString();
+                    AddUserPanel(id, name, image);
+                }
+            }
+        });
+    });
+})
