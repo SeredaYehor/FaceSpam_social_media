a8595c3f5258e9a15747d80f68a6e28682809505
Nebesnyi Andrew <73758238+artpro197@users.noreply.github.com>
2022-06-18 20:54:34 +0300

    Added messages/comments managing services

--- FaceSpam_social_media/Services/IMessageService.cs
+++ FaceSpam_social_media/Services/IMessageService.cs
@@ -1,10 +1,16 @@
 ﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using FaceSpam_social_media.Infrastructure.Data;
 
 namespace FaceSpam_social_media.Services
 {
     public interface IMessageService
     {
-        /*public Task<int> AddMessage();
-        public Task UpdateMessage();*/
+        public List<Message> GetMessages(int id, bool comments = false);
+
+        public Task<Message> AddMessage(int chatId, int userId, string message, bool comments = false);
+
+        public Task<Message> DeleteMessage(int messageId);
+
     }
 }
--- FaceSpam_social_media/Services/MessageService.cs
+++ FaceSpam_social_media/Services/MessageService.cs
@@ -5,6 +5,7 @@ using Castle.Core.Internal;
 using Microsoft.EntityFrameworkCore;
 using FaceSpam_social_media.Infrastructure.Data;
 using FaceSpam_social_media.Infrastructure.Repository;
+using System.Collections.Generic;
 
 namespace FaceSpam_social_media.Services
 {
@@ -17,13 +18,58 @@ namespace FaceSpam_social_media.Services
             _repository = repository;
         }
 
-        /*public async Task<int> AddMessage()
+        public List<Message> GetMessages(int id, bool comments = false)
         {
-            
+            List<Message> result;
+
+            if (comments)
+            {
+                result = _repository.GetAll<Message>()
+                .Where(x => x.PostPostId == id).Include(x => x.UserUser).ToList();
+            }
+            else
+            {
+                result = _repository.GetAll<Message>()
+                .Where(x => x.ChatChatId == id).Include(x => x.UserUser).ToList();
+            }
+
+            return result;
         }
-        public async Task UpdateMessage()
+
+        public async Task<Message> AddMessage(int chatId, int userId, string message, bool comments = false)
         {
-           
-        }*/
+            Message newMessage;
+
+            if (comments)
+            {
+                newMessage = await _repository.AddAsync(new Message
+                {
+                    PostPostId = chatId,
+                    UserUserId = userId,
+                    Text = message,
+                    DateSending = DateTime.Now
+                });
+            }
+            else
+            {
+                newMessage = await _repository.AddAsync(new Message
+                {
+                    ChatChatId = chatId,
+                    UserUserId = userId,
+                    Text = message,
+                    DateSending = DateTime.Now
+                });
+            }
+
+            return newMessage;
+        }
+
+        public async Task<Message> DeleteMessage(int messageId)
+        {
+            Message removedMessage = _repository.GetAll<Message>().Where(x => x.Id == messageId).First();
+            await _repository.DeleteAsync(removedMessage);
+
+            return removedMessage;
+        }
     }
 }
