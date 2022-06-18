6827276e0bf528de9d5b5e2cb3347f216f99814d
SeredaYehor <sirjeleshka@gmail.com>
2022-06-18 12:07:07 +0300

    Added scopes for db services

--- FaceSpam_social_media/Startup.cs
+++ FaceSpam_social_media/Startup.cs
@@ -32,6 +32,8 @@ namespace FaceSpam_social_media
             services.AddScoped<IRepository, EntityFrameworkRepository>();
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<IPostService, PostService>();
+            services.AddScoped<ILikeService, LikeService>();
+            services.AddScoped<IChatService, ChatService>();
             services.AddControllersWithViews().AddNewtonsoftJson(options =>
             options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
 );
