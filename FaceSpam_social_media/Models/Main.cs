df71a2ddf0c900a6746741f620a82b1b42834233
SeredaYehor <sirjeleshka@gmail.com>
2022-06-18 12:05:53 +0300

    Removed functions for workingwith db

--- FaceSpam_social_media/Models/Main.cs
+++ FaceSpam_social_media/Models/Main.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FaceSpam_social_media.Infrastructure.Data;
+using FaceSpam_social_media.Services;
 using FaceSpam_social_media.Infrastructure.Repository;
 
 namespace FaceSpam_social_media
@@ -33,55 +34,6 @@ namespace FaceSpam_social_media
             if (imageReference != null) { user.ImageReference = imageReference; }
         }
 
-        public async Task<int> AddPost(string message, string reference)
-        {
-            var newPost = await _repository.AddAsync(new Post
-            {
-                Text = message,
-                UserUserId = user.Id,
-                DatePosting = DateTime.Now
-            });
-
-            if (reference != null)
-            {
-                newPost.ImageReference = reference;
-            }
-
-            user.Posts.Add(newPost);
-            return newPost.Id;
-        }
-
-
-        private async Task RemoveChildRows(int postId, bool removeLikes)
-        {
-            if (removeLikes)
-            {
-                IEnumerable<Like> likesRemove = _repository.GetAll<Like>().Where(x => x.PostPostId == postId);
-                await _repository.DeleteAsyncRange(likesRemove);
-            }
-            else
-            {
-                List<Message> messagesRemove = _repository.GetAll<Message>().Where(x => x.PostPostId == postId).ToList();
-                await _repository.DeleteAsyncRange(messagesRemove);
-            }
-        }
-
-        public async Task<int> RemovePost(int postId)
-        {
-            user.Posts.Remove(user.Posts.Where(x => x.Id == postId && x.UserUserId == user.Id).First());
-            await RemoveChildRows(postId, false);
-            await RemoveChildRows(postId, true);
-            Post remove = _repository.GetAll<Post>().Where(x => x.Id == postId && x.UserUserId == user.Id)
-                .First();
-            await _repository.DeleteAsync(remove);
-            return remove.Id;
-        }
-
-        public void GetLikes()
-        {
-            user.Likes = _repository.GetAll<Like>().Where(x => x.PostPost.UserUserId == user.Id).ToList();
-        }
-
         public int CountLikes(int postId)
         {
             return user.Likes.Where(x => x.PostPostId == postId).Count();
@@ -92,64 +44,22 @@ namespace FaceSpam_social_media
             return user.Likes.Any(x => x.UserUserId == userId && x.PostPostId == postId);
         }
 
-        public async Task UpdatePostLike(int postId)
-        {
-            int userId = executor.Id;
-            bool liked = CheckLike(userId, postId);
-            if (liked)
-            {
-                await _repository.DeleteAsync<Like>(_repository.GetAll<Like>()
-                    .Where(x => x.UserUserId == userId && x.PostPostId == postId).First());
-                user.Likes.Remove(user.Likes.Where(x => x.UserUserId == userId && x.PostPostId == postId).First());
-            }
-            else
-            {
-                Like like = await _repository.AddAsync(new Like
-                {
-                    UserUserId = userId,
-                    PostPostId = postId
-                });
-                user.Likes.Add(like);
-            }
-        }
-
-        public void GetUser(ref User current, int userId, string name, string password)
-        {
-            if (userId != -1)
-            {
-                current = _repository.GetAll<User>().Where(x => x.Id == userId).FirstOrDefault();
-            }
-            else
-            {
-                current = _repository.GetAll<User>().Where(x => x.Name == name && x.Password == password)
-                .FirstOrDefault();
-            }
-            current.Password = "Nice try, stupid litle dum-dummy";
-        }
-
-
-        public void GetPosts()
-        {
-            user.Posts = _repository.GetAll<Post>().Where(x => x.UserUser == user)
-		.OrderByDescending(d => d.DatePosting).ToList();
-        }
-
         public void GetFriends()
         {
             friends = _repository.GetAll<Friend>().Where(x => x.UserUserId == user.Id)
                 .Select(x => x.FriendNavigation).ToList();
         }
 
-        public void GetUserInfo(bool friendDashboard, int userId, string name = null, string password = null)
+        public void GetUserInfo(IUserService userService, IPostService postService, ILikeService likeService, bool friendDashboard, int userId, string name = null, string password = null)
         {
-            GetUser(ref user, userId, name, password);
+            user = userService.GetUser(userId, name, password);
             if (!friendDashboard)
             {
                 executor = user;
             }
-            GetPosts();
+            user.Posts = postService.GetPosts(user);
             GetFriends();
-            GetLikes();
+            user.Likes = likeService.GetLikes(userId);
             IsFriend(userId);
         }
 
@@ -159,26 +69,6 @@ namespace FaceSpam_social_media
             executor.Description = change.Description;
             executor.Email = change.Email;
         }
-
-        public void GetMainUserInfo(string name, string password)
-        {
-            user = _repository.GetAll<User>().Where(x => x.Name == name && x.Password == password)
-                .FirstOrDefault();
-            GetPosts();
-            GetFriends();
-            GetLikes();
-        }
-
-        // this function gets user info by id
-        // password is cleaned
-        public void GetUserInfo(int id)
-        {
-            user = _repository.GetAll<User>().Where(x => x.Id == id).FirstOrDefault();
-            GetPosts();
-            GetFriends();
-            GetLikes();
-        }
-        // function is used to learn is friend chosen person or not
 
         public void IsFriend(int id)
         {
--- FaceSpam_social_media/Models/MessagesForm.cs
+++ FaceSpam_social_media/Models/MessagesForm.cs
@@ -83,81 +83,10 @@ namespace FaceSpam_social_media.Models
             return newMessage.Id;
         }
 
-        public void GetChats()
-        {
-            chats = _repository.GetAll<ChatMember>().Where(x => x.UserUserId == user.Id)
-                .Select(x => x.ChatChat).ToList();
-        }
-      
-         public async Task QuitGroup()
-         {
-            int count = _repository.GetAll<ChatMember>()
-                .Where(x => x.ChatChatId == selectedChat.Id).ToList().Count;
-            ChatMember chatMember = _repository.GetAll<ChatMember>()
-               .Where(x => x.ChatChatId == selectedChat.Id && x.UserUserId == user.Id).First();
-            await _repository.DeleteAsync(chatMember);
-            if (count == 1)
-            {
-                Chat remove = _repository.GetAll<Chat>().Where(x => x.Id == selectedChat.Id).First();
-                await _repository.DeleteAsync(remove);
-            }
-         }
-
-        public async Task DeleteGroup(int Id)
-        {
-            List<Message> remove_messages = _repository.GetAll<Message>().Where(x => x.ChatChatId == Id).ToList();
-            if(remove_messages.Count != 0)
-            {
-                await _repository.DeleteAsyncRange(remove_messages);
-            }
-            Chat remove =  chats.Where(x => x.Id == Id).FirstOrDefault();
-            await _repository.DeleteAsync(remove);
-            chats.Remove(remove);
-        }
-
         public void SelectChat(int chatId)
         {
             selectedChat = chats.Where(x => x.Id == chatId).First();
             GetChatMessages(chatId);
-        }
-
-        public List<User> SelectAllUsers()
-        {
-            List<User> users =  _repository.GetAll<User>().Where(x => x.Id != user.Id).ToList();
-            return users;
-        }
-
-        public async Task<Chat> CreateGroup(string name, string description, List<int> members, string reference)
-        {
-            Chat created = new Chat()
-            {
-                ChatName = name,
-                Description = description,
-                DateCreating = DateTime.Now,
-                Admin = user.Id,
-            };
-            if (reference != null)
-            {
-                created.ImageReference = reference;
-            }
-            else
-            {
-                created.ImageReference = "../Images/default_group.jpg";
-            }
-            var newChat = await _repository.AddAsync(created);
-            chats.Add(newChat);
-            List<ChatMember> newMembers = new List<ChatMember>();
-            foreach (int member in members)
-            {
-                ChatMember newMember = new ChatMember() 
-                { 
-                    ChatChatId = created.Id, 
-                    UserUserId = member 
-                };
-                newMembers.Add(newMember);
-            }
-            await _repository.AddAsyncRange(newMembers);
-            return newChat;
         }
     }
 }
