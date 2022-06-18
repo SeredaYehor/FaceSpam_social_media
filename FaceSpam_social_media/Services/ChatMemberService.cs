16c86e017693640ea6f4c90bb119b4fbafcff5e8
SeredaYehor <sirjeleshka@gmail.com>
2022-06-18 13:01:19 +0300

    Added realisation of chatMember managment db service

---
+++ FaceSpam_social_media/Services/ChatMemberService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FaceSpam_social_media.Infrastructure.Data;
+using FaceSpam_social_media.Infrastructure.Repository;
+
+namespace FaceSpam_social_media.Services
+{
+    public class ChatMemberService : IChatMemberService
+    {
+        private readonly IRepository _repository;
+
+        public ChatMemberService(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public List<User> GetChatMembers(int chatId)
+            => _repository.GetAll<ChatMember>().Where(x => x.ChatChatId == chatId)
+            .Select(y => y.UserUser).ToList();
+
+        public async Task<int> AddChatMember(int chatId, int memberId)
+        {
+            ChatMember newMember = new ChatMember
+            {
+                ChatChatId = chatId,
+                UserUserId = memberId
+            };
+            await _repository.AddAsync<ChatMember>(newMember);
+            return GetChatMembers(chatId).Count;
+        }
+
+        public async Task<int> RemoveChatMember(int chatId, int memberId)
+        {
+            ChatMember remove = _repository.GetAll<ChatMember>().Where(x => x.UserUserId == memberId
+            && x.ChatChatId == chatId).First();
+            await _repository.DeleteAsync(remove);
+            return GetChatMembers(chatId).Count;
+        }
+    }
+}
---
+++ FaceSpam_social_media/Services/IChatMemberService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FaceSpam_social_media.Infrastructure.Data;
+
+namespace FaceSpam_social_media.Services
+{
+    public interface IChatMemberService
+    {
+        public List<User> GetChatMembers(int chatId);
+
+        public Task<int> AddChatMember(int chatId, int memberId);
+
+        public Task<int> RemoveChatMember(int chatId, int memberId);
+    }
+}
