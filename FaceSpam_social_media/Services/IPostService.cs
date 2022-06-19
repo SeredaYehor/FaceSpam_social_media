82d5b05619a70c27f67bb881866adbb421ed2702
SeredaYehor <sirjeleshka@gmail.com>
2022-06-19 13:34:27 +0300

    Fixed adding posts with images

---
+++ FaceSpam_social_media/Services/IPostService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using FaceSpam_social_media.Infrastructure.Data;
+
+namespace FaceSpam_social_media.Services
+{
+    public interface IPostService
+    {
+        public List<Post> GetPosts(User user);
+        public Task<int> AddPost(int userId, string message, string reference);
+        public Task RemoveChildRows(int postId, bool removeLikes);
+        public Task<int> RemovePost(int userId, int postId);
+    }
+}
