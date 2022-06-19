7df2e16860521843cc4cdb75725c8b0c8c520919
Nebesnyi Andrew <73758238+artpro197@users.noreply.github.com>
2022-06-19 11:53:01 +0300

    Changed all Friends functions to Follow functions

---
+++ FaceSpam_social_media/wwwroot/scripts/FollowsFormScripts.js
@@ -0,0 +1,51 @@
+﻿$(document).ready(function () {
+
+    $(".Search").keyup(function () {
+        var search = this.value;
+        if (search.trim() == '') {
+            $(".FriendLine").show();
+        }
+        else {
+            $(".FriendLine").hide().filter('[id^=' + search + ']').show();
+        }
+    });
+});
+
+function DeleteFollow(obj, friendPage = false) {
+
+    var user = obj.id;
+    $.ajax({
+        type: "GET",
+        url: "/Home/DeleteFollow",
+        async: false,
+        data: { id: user, },
+        success: function () {
+            if (friendPage) {
+                var parentDiv = $(obj).parent();
+                $(parentDiv).remove();
+            }
+        }
+    });
+}
+
+function AddFollow(obj) {
+    var user = obj.id;
+
+    $.ajax({
+        type: "GET",
+        url: "/Home/AddFollow",
+        async: false,
+        data: { id: user, },
+    });
+}
+
+function CheckAction(obj) {
+    if (obj.value == "Remove") {
+        obj.value = "Follow";
+        DeleteFollow(obj);
+    }
+    else {
+        obj.value = "Remove";
+        AddFollow(obj);
+    }
+}
