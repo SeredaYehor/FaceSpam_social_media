1f00bdc82095b1b83cd9a92b5d61b278abfd5842
SeredaYehor <sirjeleshka@gmail.com>
2022-06-19 11:58:36 +0300

    Added signalR messages hub

---
+++ FaceSpam_social_media/SignalRHub/MessagesHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FaceSpam_social_media.Infrastructure.Data;
+
+namespace FaceSpam_social_media.SignalRHub
+{
+    public class MessagesHub : Hub
+    {
+    }
+}
