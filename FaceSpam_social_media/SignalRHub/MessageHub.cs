0e4802cfcaa5eaf7ac5bc5c6e9143d362669085b
Nebesnyi Andrew <73758238+artpro197@users.noreply.github.com>
2022-07-23 22:54:07 +0300

    Added hub methods to trigger send/delete message functions

--- FaceSpam_social_media/SignalRHub/MessageHub.cs
+++ FaceSpam_social_media/SignalRHub/MessageHub.cs
@@ -8,5 +8,15 @@ namespace FaceSpam_social_media.SignalRHub
 {
     public class MessageHub : Hub
     {
+        public async Task Send(int messageId, string message, string userName, string image, int selectedChat)
+        {
+            await this.Clients.All.SendAsync("Send", messageId, message, userName, image, selectedChat);
+        }
+
+        public async Task Remove(int messageId)
+        {
+            await this.Clients.All.SendAsync("Remove", messageId);
+        }
+
     }
 }
