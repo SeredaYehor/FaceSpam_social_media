30817fa2232d293e5e8d473365013ec2b679a916
Nebesnyi Andrew <73758238+artpro197@users.noreply.github.com>
2022-07-23 22:15:01 +0300

    Connected SignalR services, created hubs for comments and message pages

---
+++ FaceSpam_social_media/SignalRHub/MessageHub.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace FaceSpam_social_media.SignalRHub
+{
+    public class MessageHub : Hub
+    {
+    }
+}
